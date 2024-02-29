4c26e701a46b732bee1cb6074d6f3f9db264a04f
sooraj planku veedu <sooraj.planku@enpal.de>
2024-02-29 18:54:45 +0100

    adding files to repo first commit

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://flask_user:password@localhost/flask_api_db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.name}>'
+
+@app.route('/users', methods=['GET', 'POST'])
+def users():
+    if request.method == 'GET':
+        users = User.query.all()
+        users_list = []
+        for user in users:
+            users_list.append({'id': user.id, 'name': user.name})
+        return jsonify(users_list)
+    
+    elif request.method == 'POST':
+        data = request.get_json()
+        new_user = User(name=data['name'])
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify({'message': 'User added successfully'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
